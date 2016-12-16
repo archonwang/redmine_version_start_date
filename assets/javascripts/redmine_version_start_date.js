e1c45d2893064d979c5871ed444470603d9561ed
Akito Michishita <routetheta@gmail.com>
2016-12-17 00:28:50 +0900

    ロードマップ詳細ページの期日の表記を変更

--- assets/javascripts/redmine_version_start_date.js
+++ assets/javascripts/redmine_version_start_date.js
@@ -1,5 +1,6 @@
 $(function(){
-  var $org_elements = $('h3.version + p');
+  var $org_elements =
+    $('.controller-versions.action-index h3.version + p, .controller-versions.action-show #roadmap > p:first-child');
   var $mod_elements = $('.redmine_version_start_date');
   $.each($mod_elements, function(i, mod_element){
     if(!$(mod_element).hasClass('none')){
