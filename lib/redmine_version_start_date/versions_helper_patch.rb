469ee87463df1661e8f7acae8338929bf9438ea7
Akito Michishita <routetheta@gmail.com>
2017-01-13 18:57:28 +0900

    helperに関数追加

--- lib/redmine_version_start_date/versions_helper_patch.rb
+++ lib/redmine_version_start_date/versions_helper_patch.rb
@@ -9,6 +9,14 @@ module RedmineVersionStartDate
         locale.t(:label_roadmap_start_in, value: distance_of_date_in_words(Time.zone.today, date))
       end if date.present? && date > Time.zone.today
     end
+
+    def version_period(version)
+      if version.start_date.present? && version.effective_date.present?
+        "#{format_date(version.start_date)} - #{format_date(version.effective_date)}"
+      elsif version.effective_date.present?
+        format_date(version.effective_date)
+      end
+    end
   end
 
   ActionDispatch::Reloader.to_prepare do
